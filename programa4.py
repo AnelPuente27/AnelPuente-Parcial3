c73861cf2eed34381af3e1f7478b3ea0e20dba0d
Anel Puente <156528840+AnelPuente27@users.noreply.github.com>
2024-04-13 10:39:12 -0600

    Examen
    Programa4 es el examen

--- programa4.py
+++ programa4.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import pywhatkit
 
 app = Flask(__name__)
 
@@ -9,9 +10,21 @@ tasks = [
     {"id":  4, "name":"Zero", "enable": False},
 ]
 
-@app.route('/tasks' , methods=['GET'])
-def get_tasks():
-    return jsonify(tasks)
+@app.route('/whatapp-message', methods=['GET', 'POST'])
+def whatsapp_example():
+    # handle the POST request
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'message' in data:
+            message = data['message']
+            try:
+                pywhatkit.sendwhatmsg_to_group_instantly("HMSd7sLfFPy4ivP0YtAqvE", message)
+                return 'message sended sucessfully to group', 200
+            except Exception as e:
+                print("Error:", e)
+                return 'Error sending message', 500
+        else:
+            return 'Message not found in request body', 400
 
 if __name__ == '__main__':
     app.run(debug=True)
